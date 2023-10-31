87cd78c6b191126ba6b8460925579d24e3a5706f
coconoca-8039 <coconoca.8039@gmail.com>
2023-11-01 06:47:31 +0900

    arduino改善

--- ARDUINO/dont_use_delay_for_LED_blinking.ino/dont_use_delay_for_LED_blinking.ino/dont_use_delay_for_LED_blinking/dont_use_delay_for_LED_blinking.ino
+++ ARDUINO/dont_use_delay_for_LED_blinking.ino/dont_use_delay_for_LED_blinking.ino/dont_use_delay_for_LED_blinking/dont_use_delay_for_LED_blinking.ino
@@ -3,11 +3,17 @@
 #define LED2 5
 #define LED3 6
 #define LED4 13
+#define MAX_CASE 3  // LEDの最大個数
+#define UPDATE_INTERVAL 10  // LEDの点滅周期 10で1秒 100で10秒
+
+#define NUM_LEDS 3  // LEDの数
+int ledPins[NUM_LEDS] = {3, 5, 6};
 
 // プロトタイプ宣言
-void blink_1sec_led(int currentCase);
+void cycleLEDs(int currentCase);
 void updateCurrentCase(int &currentCase);
 void blink_led13(unsigned char &count, int &state);
+int blink(int pin, int state);
 
 void setup(){
     // LEDを出力モードに設定
@@ -26,6 +32,7 @@ void loop(){
     static int state = HIGH;
 
     //100ミリ秒経過したら次の処理に移行する
+
     while ((millis() - millis_buf) < 100){
         ;
     }
@@ -34,43 +41,54 @@ void loop(){
 
     count++;
 
-    if (count % 10 == 0) {
+    if (count % UPDATE_INTERVAL == 0) {
         updateCurrentCase(currentCase);
     }
 
-    blink_1sec_led(currentCase);  // LED3つの制御
+    //cycleLEDs(currentCase);  // LED3つの制御
+    toggleLEDs(currentCase);
     blink_led13(count, state);
 }
 
 void updateCurrentCase(int &currentCase) {
     currentCase++;
-    if (currentCase > 2) {
+    if (currentCase > MAX_CASE) {
         currentCase = 0;
     }
 }
 
-void blink_1sec_led(int currentCase){
-   switch(currentCase){
-        case 0:
-            digitalWrite(LED1, HIGH);
-            digitalWrite(LED2, LOW);
-            digitalWrite(LED3, LOW);
-            break;
-        case 1:
-            digitalWrite(LED1, LOW);
-            digitalWrite(LED2, HIGH);
-            digitalWrite(LED3, LOW);
-            break;
-        case 2:
-            digitalWrite(LED1, LOW);
-            digitalWrite(LED2, LOW);
-            digitalWrite(LED3, HIGH);
-            break;
-    } 
+// void cycleLEDs(int currentCase){
+//    switch(currentCase){
+//         case 0:
+//             digitalWrite(LED1, HIGH);
+//             digitalWrite(LED2, LOW);
+//             digitalWrite(LED3, LOW);
+//             break;
+//         case 1:
+//             digitalWrite(LED1, LOW);
+//             digitalWrite(LED2, HIGH);
+//             digitalWrite(LED3, LOW);
+//             break;
+//         case 2:
+//             digitalWrite(LED1, LOW);
+//             digitalWrite(LED2, LOW);
+//             digitalWrite(LED3, HIGH);
+//             break;
+//     } 
+// }
+
+void toggleLEDs(int currentCase){
+    for (int i = 0; i < NUM_LEDS; i++) {
+        if (i == currentCase) {
+            digitalWrite(ledPins[i], HIGH);  // 点灯
+        } else {
+            digitalWrite(ledPins[i], LOW);  // 消灯
+        }
+    }
 }
 
 void blink_led13(unsigned char &count, int &state){
-    if((count % 10) == 0){
+    if((count % UPDATE_INTERVAL) == 0){
       state = blink(LED4, state);
     }
 }
